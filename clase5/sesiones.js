287c43ddaf19b2713f6cc293ed877cc07934aaca
juancotera <juancoteracastro@gmail.com>
2023-09-16 17:18:46 -0500

    clase5

---
+++ clase5/sesiones.js
@@ -0,0 +1,22 @@
+// let suma = (a, b) => a + b;
+// console.log(suma(2, 2));
+
+// function dameCinco() {
+//     return [1,2,3,4,5];
+// }
+
+// let dameCinco = () => [1,2,3,4,5]
+
+// function multiplicarPorDos() {
+//   return 123 * 2 ;
+// }
+
+
+let multiplicarPorDos = (a, b) => a * b;
+console.log(multiplicarPorDos(123, 2))
+
+// function mostrarNombre() {
+//   return "Mi nombre es Hernán";
+// }
+
+// let mostrarNombre = (a) => 'Mi nombre es ' + a;
