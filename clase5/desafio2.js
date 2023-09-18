142b913b4b0cf89f5cf3f09c2194eb29810f0703
juancotera <juancoteracastro@gmail.com>
2023-09-18 16:36:07 -0500

    ..

---
+++ clase5/desafio2.js
@@ -0,0 +1,25 @@
+//hamburguesas
+let tipoHamburguesa = 1800;
+let pollo = 1500;
+let vegetariana = 1200;
+//adicionales
+let jamon = (a) => a == true ? a = 20 : " ";
+let queso = true;
+let salsaDeTomate = true;
+let mayonesa = true;
+let mostaza = true;
+let tomate = true;
+let lechuga = true;
+let cebolla = true;
+
+
+// let totalPagar = (b,c,d,e,f,g,h,i, tipo) => tipo(b + c + d + e + f + g + h + i)
+
+
+function totalPagar(a,b) {
+  queso == true 
+}
+
+
+
+
