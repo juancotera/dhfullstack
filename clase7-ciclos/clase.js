6ec4486a828d68aaa607e6645743f696079b62dc
juancotera <juancoteracastro@gmail.com>
2023-09-30 08:47:30 -0500

    ..

--- clase7-ciclos/clase.js
+++ clase7-ciclos/clase.js
@@ -1,4 +1,16 @@
-for(let i = 0; i <= 10; i++){
-  console.log(i)
+// for(let i = 0; i <= 10; i++){
+//   console.log(i)
+// }
+
+function noParesDeContarImparesHasta(numero) {
+    // Escribe aqui tu código
+    let cuantos = 0
+    for (let i = 0; i <= numero; i++){
+      if((i % 2) !== 0){
+        cuantos = cuantos + i
+      }
+    }
+    return cuantos
 }
+console.log(noParesDeContarImparesHasta(5))
 
