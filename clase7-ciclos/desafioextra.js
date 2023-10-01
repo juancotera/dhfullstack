e210fa68df44a54ae6f7df6375f39d8b6fbbfc97
juancotera <juancoteracastro@gmail.com>
2023-10-01 13:18:22 -0500

    d extra

---
+++ clase7-ciclos/desafioextra.js
@@ -0,0 +1,33 @@
+//1 Crear una función que pida un valor por parámetro y muestre los 10 números siguientes.
+// function mostrarSiguientes(numero){
+//   let limite = numero + 10
+//   for(let i = numero; i <= limite; i++){
+//     console.log(i)
+//   }
+// }
+
+// mostrarSiguientes(13);
+
+//2 Imprimir los números entre el 1 y el 57, saltando de tres en tres.
+// function saltandoEnTres(){
+//   for(let i = 0; i <= 57;i = i + 3){
+//     console.log(i)
+//   }
+// }
+
+// saltandoEnTres()
+
+//3 Escribir un programa que muestre la sumatoria de todos los números entre el 0 y el 100.
+// function sumaTodos(){
+//   let suma = 0
+//   for(let i = 0; i <= 100; i++){
+//   //console.log('ahora estoy sumando ' + suma + '+' + i )
+//     suma = suma + i;
+//   //console.log(' y es = ' + suma)
+//   }
+//   console.log(suma);
+// }
+
+// sumaTodos();
+
+//4
