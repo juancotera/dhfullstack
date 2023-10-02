498ab26fb33e5e57ac3ae03690552575f240e6ac
juancotera <juancoteracastro@gmail.com>
2023-10-02 18:00:28 -0500

    desafios extras terminados

--- clase7-ciclos/desafioextra.js
+++ clase7-ciclos/desafioextra.js
@@ -9,13 +9,13 @@
 // mostrarSiguientes(13);
 
 // 2 Imprimir los números entre el 1 y el 57, saltando de tres en tres.
-function saltandoEnTres(){
-  for(let i = 0; i <= 57;i += 3){
-    console.log(i)
-  }
-}
+// function saltandoEnTres(){
+//   for(let i = 0; i <= 57;i += 3){
+//     console.log(i)
+//   }
+// }
 
-saltandoEnTres()
+// saltandoEnTres()
 
 //3 Escribir un programa que muestre la sumatoria de todos los números entre el 0 y el 100.
 // function sumaTodos(){
@@ -30,4 +30,37 @@ saltandoEnTres()
 
 // sumaTodos();
 
-//4
+//4 Crear una función que reciba por parámetro una cadena de texto: ‘practicando el uso
+// de los ciclos o bucles’. Tendrá la responsabilidad de mostrar al usuario cada una de
+// las letras de la cadena de texto pero en MAYÚSCULA.
+// Pista: Investigar el método .toUpperCase.
+
+// function miTexto(string){
+//   let contador = 0
+//   for (let i = 1; i <= string.length; i++){
+//     let tomaLetras = string.slice(contador, i)
+//     let mayusculas  = tomaLetras.toUpperCase()
+//     console.log(mayusculas);
+//     contador++
+//   }
+// }
+
+// miTexto("Hola Mundo");
+
+//5 Crear una función que reciba como parámetro un array de números positivos. Tendrá
+// la responsabilidad de retornar un nuevo array pero sólo con los valores pares.
+// Pista: Investiga sobre el uso del operador módulo. Recordemos también que un
+// número par es aquel que se puede dividir entre 2.
+
+// function misNumeros(array){
+//   let guardado = []
+//   for(let i = 0; i <= array.length; i++){
+//     let numero = array[i]
+//     if(numero % 2 == 0){
+//       guardado.push(numero)
+//     }
+//   }
+//   return console.log(guardado)
+// }
+
+// misNumeros([43,56,80,25,32])
